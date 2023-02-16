63e23e2aa4469ee586107ecb199e965498cd8828
swmac <shaynefw@gmail.com>
2023-02-16 18:42:41 -0500

    added save note functionality

--- routes/getRoute.js
+++ routes/getRoute.js
@@ -6,13 +6,20 @@ const notes = new Notes(); // Create a new instance of the Notes class
 
 // GET /notes - returns the notes.html file
 router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 // GET /api/notes - reads the db.json file and returns all saved notes as JSON
 router.get("/api/notes", async (req, res) => {
   const savedNotes = await notes.getNotes();
-  res.json(savedNotes); 
+  res.json(savedNotes);
+});
+
+// POST /api/notes - receives a new note to save on the request body, adds it to the db.json file, and then returns the new note to the client
+router.post("/api/notes", async (req, res) => {
+  const newNote = req.body; 
+  const savedNote = await notes.addNote(newNote); 
+  res.json(savedNote); 
 });
 
 module.exports = router;
